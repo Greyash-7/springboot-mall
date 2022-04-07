094cc6eae5905c7cc8b5f3f2944058e3648871d5
Grey <1638346448@qq.com>
2022-04-07 13:57:45 +0800

    10.

--- mall.sql
+++ mall.sql
@@ -14,18 +14,18 @@ CREATE TABLE `mall_category` (
 
 INSERT INTO `mall_category` (`id`, `parent_id`, `name`, `status`, `sort_order`, `create_time`, `update_time`)
 VALUES
-	(100001,0,'家用电器',1,1,'2000-03-25 16:46:00','2000-03-25 16:46:00'),
-	(100002,0,'数码3C',1,1,'2000-03-25 16:46:21','2000-03-25 16:46:21'),
-	(100003,0,'服装箱包',1,1,'2000-03-25 16:49:53','2000-03-25 16:49:53'),
+	(100001,0,'脏腑调理',1,1,'2000-03-25 16:46:00','2000-03-25 16:46:00'),
+	(100002,0,'疾病调理',1,1,'2000-03-25 16:46:21','2000-03-25 16:46:21'),
+	(100003,0,'功能性调理',1,1,'2000-03-25 16:49:53','2000-03-25 16:49:53'),
 	(100004,0,'食品生鲜',1,1,'2000-03-25 16:50:19','2000-03-25 16:50:19'),
 	(100005,0,'酒水饮料',1,1,'2000-03-25 16:50:29','2000-03-25 16:50:29'),
-	(100006,100001,'冰箱',1,1,'2000-03-25 16:52:15','2000-03-25 16:52:15'),
-	(100007,100001,'电视',1,1,'2000-03-25 16:52:26','2000-03-25 16:52:26'),
-	(100008,100001,'洗衣机',1,1,'2000-03-25 16:52:39','2000-03-25 16:52:39'),
-	(100009,100001,'空调',1,1,'2000-03-25 16:52:45','2000-03-25 16:52:45'),
-	(100010,100001,'电热水器',1,1,'2000-03-25 16:52:54','2000-03-25 16:52:54'),
-	(100011,100002,'电脑',1,1,'2000-03-25 16:53:18','2000-03-25 16:53:18'),
-	(100012,100002,'手机',1,1,'2000-03-25 16:53:27','2000-03-25 16:53:27'),
+	(100006,100001,'补心',1,1,'2000-03-25 16:52:15','2000-03-25 16:52:15'),
+	(100007,100001,'养肝',1,1,'2000-03-25 16:52:26','2000-03-25 16:52:26'),
+	(100008,100001,'补脾',1,1,'2000-03-25 16:52:39','2000-03-25 16:52:39'),
+	(100009,100001,'养肺',1,1,'2000-03-25 16:52:45','2000-03-25 16:52:45'),
+	(100010,100001,'补肾',1,1,'2000-03-25 16:52:54','2000-03-25 16:52:54'),
+	(100011,100001,'补气',1,1,'2000-03-25 16:53:18','2000-03-25 16:53:18'),
+	(100012,100001,'补血',1,1,'2000-03-25 16:53:27','2000-03-25 16:53:27'),
 	(100013,100002,'平板电脑',1,1,'2000-03-25 16:53:35','2000-03-25 16:53:35'),
 	(100014,100002,'数码相机',1,1,'2000-03-25 16:53:56','2000-03-25 16:53:56'),
 	(100015,100002,'3C配件',1,1,'2000-03-25 16:54:07','2000-03-25 16:54:07'),
@@ -116,10 +116,11 @@ CREATE TABLE `mall_product` (
 
 INSERT INTO `mall_product` (`id`, `category_id`, `name`, `subtitle`, `main_image`, `sub_images`, `detail`, `price`, `stock`, `status`, `create_time`, `update_time`)
 VALUES
-	(26,100002,'Apple iPhone 7 Plus (A1661) 128G 玫瑰金色 移动联通电信4G手机','iPhone 7，现更以红色呈现。','http://img.springboot.cn/241997c4-9e62-4824-b7f0-7425c3c28917.jpeg','241997c4-9e62-4824-b7f0-7425c3c28917.jpeg,b6c56eb0-1748-49a9-98dc-bcc4b9788a54.jpeg,92f17532-1527-4563-aa1d-ed01baa0f7b2.jpeg,3adbe4f7-e374-4533-aa79-cc4a98c529bf.jpeg','<p><img alt=\"10000.jpg\" src=\"http://img.springboot.cn/00bce8d4-e9af-4c8d-b205-e6c75c7e252b.jpg\" width=\"790\" height=\"553\"><br></p><p><img alt=\"20000.jpg\" src=\"http://img.springboot.cn/4a70b4b4-01ee-46af-9468-31e67d0995b8.jpg\" width=\"790\" height=\"525\"><br></p><p><img alt=\"30000.jpg\" src=\"http://img.springboot.cn/0570e033-12d7-49b2-88f3-7a5d84157223.jpg\" width=\"790\" height=\"365\"><br></p><p><img alt=\"40000.jpg\" src=\"http://img.springboot.cn/50515c02-3255-44b9-a829-9e141a28c08a.jpg\" width=\"790\" height=\"525\"><br></p><p><img alt=\"50000.jpg\" src=\"http://img.springboot.cn/c138fc56-5843-4287-a029-91cf3732d034.jpg\" width=\"790\" height=\"525\"><br></p><p><img alt=\"60000.jpg\" src=\"http://img.springboot.cn/c92d1f8a-9827-453f-9d37-b10a3287e894.jpg\" width=\"790\" height=\"525\"><br></p><p><br></p><p><img alt=\"TB24p51hgFkpuFjSspnXXb4qFXa-1776456424.jpg\" src=\"http://img.springboot.cn/bb1511fc-3483-471f-80e5-c7c81fa5e1dd.jpg\" width=\"790\" height=\"375\"><br></p><p><br></p><p><img alt=\"shouhou.jpg\" src=\"http://img.springboot.cn/698e6fbe-97ea-478b-8170-008ad24030f7.jpg\" width=\"750\" height=\"150\"><br></p><p><img alt=\"999.jpg\" src=\"http://img.springboot.cn/ee276fe6-5d79-45aa-8393-ba1d210f9c89.jpg\" width=\"790\" height=\"351\"><br></p>',6999.00,96,1,'2000-04-13 20:45:41','2000-04-13 21:45:41'),
-	(27,100006,'Midea/美的 BCD-535WKZM(E)冰箱双开门对开门风冷无霜智能电家用','送品牌烤箱，五一大促','http://img.springboot.cn/ac3e571d-13ce-4fad-89e8-c92c2eccf536.jpeg','ac3e571d-13ce-4fad-89e8-c92c2eccf536.jpeg,4bb02f1c-62d5-48cc-b358-97b05af5740d.jpeg,36bdb49c-72ae-4185-9297-78829b54b566.jpeg','<p><img alt=\"miaoshu.jpg\" src=\"http://img.springboot.cn/9c5c74e6-6615-4aa0-b1fc-c17a1eff6027.jpg\" width=\"790\" height=\"444\"><br></p><p><img alt=\"miaoshu2.jpg\" src=\"http://img.springboot.cn/31dc1a94-f354-48b8-a170-1a1a6de8751b.jpg\" width=\"790\" height=\"1441\"><img alt=\"miaoshu3.jpg\" src=\"http://img.springboot.cn/7862594b-3063-4b52-b7d4-cea980c604e0.jpg\" width=\"790\" height=\"1442\"><img alt=\"miaoshu4.jpg\" src=\"http://img.springboot.cn/9a650563-dc85-44d6-b174-d6960cfb1d6a.jpg\" width=\"790\" height=\"1441\"><br></p>',3299.00,99,1,'2000-04-13 18:51:54','2000-04-13 21:45:41'),
-	(28,100012,'4+64G送手环/Huawei/华为 nova 手机P9/P10plus青春','NOVA青春版1999元','http://img.springboot.cn/0093f5d3-bdb4-4fb0-bec5-5465dfd26363.jpeg','0093f5d3-bdb4-4fb0-bec5-5465dfd26363.jpeg,13da2172-4445-4eb5-a13f-c5d4ede8458c.jpeg,58d5d4b7-58d4-4948-81b6-2bae4f79bf02.jpeg','<p><img alt=\"11TB2fKK3cl0kpuFjSsziXXa.oVXa_!!1777180618.jpg\" src=\"http://img.springboot.cn/5c2d1c6d-9e09-48ce-bbdb-e833b42ff664.jpg\" width=\"790\" height=\"966\"><img alt=\"22TB2YP3AkEhnpuFjSZFpXXcpuXXa_!!1777180618.jpg\" src=\"http://img.springboot.cn/9a10b877-818f-4a27-b6f7-62887f3fb39d.jpg\" width=\"790\" height=\"1344\"><img alt=\"33TB2Yyshk.hnpuFjSZFpXXcpuXXa_!!1777180618.jpg\" src=\"http://img.springboot.cn/7d7fbd69-a3cb-4efe-8765-423bf8276e3e.jpg\" width=\"790\" height=\"700\"><img alt=\"TB2diyziB8kpuFjSspeXXc7IpXa_!!1777180618.jpg\" src=\"http://img.springboot.cn/1d7160d2-9dba-422f-b2a0-e92847ba6ce9.jpg\" width=\"790\" height=\"393\"><br></p>',1999.00,100,1,'2000-04-13 18:57:18','2000-04-13 21:45:41'),
-	(29,100008,'Haier/海尔HJ100-1HU1 10公斤滚筒洗衣机全自动带烘干家用大容量 洗烘一体','门店机型 德邦送货','http://img.springboot.cn/173335a4-5dce-4afd-9f18-a10623724c4e.jpeg','173335a4-5dce-4afd-9f18-a10623724c4e.jpeg,42b1b8bc-27c7-4ee1-80ab-753d216a1d49.jpeg,2f1b3de1-1eb1-4c18-8ca2-518934931bec.jpeg','<p><img alt=\"1TB2WLZrcIaK.eBjSspjXXXL.XXa_!!2114960396.jpg\" src=\"http://img.springboot.cn/ffcce953-81bd-463c-acd1-d690b263d6df.jpg\" width=\"790\" height=\"920\"><img alt=\"2TB2zhOFbZCO.eBjSZFzXXaRiVXa_!!2114960396.jpg\" src=\"http://img.springboot.cn/58a7bd25-c3e7-4248-9dba-158ef2a90e70.jpg\" width=\"790\" height=\"1052\"><img alt=\"3TB27mCtb7WM.eBjSZFhXXbdWpXa_!!2114960396.jpg\" src=\"http://img.springboot.cn/2edbe9b3-28be-4a8b-a9c3-82e40703f22f.jpg\" width=\"790\" height=\"820\"><br></p>',4299.00,100,1,'2000-04-13 19:07:47','2000-04-13 21:45:41');
+    (26,100006,'凉拌牛心','主料：牛心','https://st-cn.meishij.net/r/175/213/490925/s490925_142021040462101.jpg','','',999.00,999,1,'2022-04-13 19:07:47','2022-04-13 21:45:41'),
+    (27,100007,'养肝护肝粥','主料：胚芽米','https://st-cn.meishij.net/r/169/94/8586169/s8586169_155694928259338.jpg','','',999.00,999,1,'2022-04-13 19:07:47','2022-04-13 21:45:41'),
+    (28,100008,'健脾补虚粥','主料：莲子、芡实、薏米、淮山药、桂圆、白扁豆、红枣、粳米','https://st-cn.meishij.net/r/184/229/5557434/s5557434_147123299221965.jpg','','',999.00,999,1,'2022-04-13 19:07:47','2022-04-13 21:45:41'),
+    (29,100009,'秋季养肺黄金粥','主料：黄金粥、红枣、黑玉米','https://st-cn.meishij.net/r/80/238/5684580/s5684580_150675018356347.jpg','','',999.00,999,1,'2022-04-13 19:07:47','2022-04-13 21:45:41'),
+    (30,100011,'黄芪红枣补气汤','主料：油鸡、黄芪、红枣','https://st-cn.meishij.net/rs/105/225/1306355/n1306355_155858047996597.jpg','','',999.00,999,1, '2022-04-13 19:07:47','2022-04-13 21:45:41');
 
 DROP TABLE IF EXISTS `mall_shipping`;
 CREATE TABLE `mall_shipping` (
